5a1d2b20efb210f23a93faeff60580f43e84d89a
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-09-26 16:47:47 +0200

    sesion-02:ej01.js finalizado

---
+++ sesion-02/ej01.js
@@ -0,0 +1,16 @@
+const persona = {
+    nombre: "Luis",
+    edad: 25,
+    profesion: "ingeniero",
+   };
+   // Desestructuración básica de objeto
+   const { nombre, edad } = persona;
+   console.log(`- ej01.js`);
+   console.log(`- ej01.js`);
+   console.log(``);
+   console.log(`>> SALIDA `);
+   console.log(`>> SALIDA `);
+   console.log(``);
+   console.log(nombre); // Luis
+   console.log(edad); // 25
+   
