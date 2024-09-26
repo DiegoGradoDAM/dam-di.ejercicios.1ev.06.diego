9b6d4501349032827473b20f806eb6f232e2bfd5
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-09-26 17:51:52 +0200

    sesion-02:ej04.js finalizado

---
+++ sesion-02/ej04.js
@@ -0,0 +1,12 @@
+const numeros = [1, 2, 3, 4, 5];
+
+const [primero, ...resto] = numeros;
+
+console.log(`- ej04.js`);
+console.log(`- ej04.js`);
+console.log(``);
+console.log(`>> SALIDA `);
+console.log(`>> SALIDA `);
+console.log(``);
+console.log("Primer numero: "+primero);
+console.log("Resto de numeros: "+resto);
