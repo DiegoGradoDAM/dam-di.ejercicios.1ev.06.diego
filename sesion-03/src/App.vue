08f305e5e504c88b4a0baa377bd6a107e4f8d1b7
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-10-03 18:22:32 +0200

    Ejercicio terminado

--- sesion-03/src/App.vue
+++ sesion-03/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
   import MensajeEstatico from './components/MensajeEstatico.vue';
   import ListaEstatica from './components/ListaEstatica.vue';
+  import LibroInfo from './components/LibroInfo.vue';
 </script>
 
 <template>
   <h2>EJ02AP01: Aplicacion inicializada.</h2>
   <MensajeEstatico></MensajeEstatico>
   <ListaEstatica></ListaEstatica>
+  <LibroInfo></LibroInfo>
 </template>
 
 <style scoped>
---
+++ sesion-03/src/components/LibroInfo.vue
@@ -0,0 +1,12 @@
+<script setup>
+    const titulo = "Titulo";
+    const autor = "Autor";
+    const anyo = 2024;
+</script>
+<template>
+    <p>{{titulo}}</p>
+    <p>{{ autor }}</p>
+    <p>{{ anyo }}</p>
+</template>
+<style scoped>
+</style>
