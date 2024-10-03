38058fce8084c110c1cc6ba55563ad4eb0c60585
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-10-03 18:50:07 +0200

    Ejercicio terminado

---
+++ sesion-03/src/components/ListaTitulos.vue
@@ -0,0 +1,10 @@
+<script setup>
+    const array = ["Bienvenido", "Introduccion", "Conclusión"];
+</script>
+<template>
+   <h2>{{ array[0] }}</h2>
+   <h2>{{ array[1] }}</h2>
+   <h2>{{ array[2] }}</h2>
+</template>
+<style scoped>
+</style>
