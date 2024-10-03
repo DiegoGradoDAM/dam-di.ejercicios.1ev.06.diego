f08f6dcdd1906b08924bc9f74775f6e8f1a141f3
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-10-03 17:41:19 +0200

    Ejercicio terminado

--- sesion-03/src/App.vue
+++ sesion-03/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+  import MensajeEstatico from './components/MensajeEstatico.vue';
 </script>
 
 <template>
   <h2>EJ02AP01: Aplicacion inicializada.</h2>
+  <MensajeEstatico></MensajeEstatico>
 </template>
 
 <style scoped>
---
+++ sesion-03/src/components/MensajeEstatico.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+    <h2>Bienvenido al curso de Vue 3.</h2>
+</template>
+<style scoped>
+</style>
