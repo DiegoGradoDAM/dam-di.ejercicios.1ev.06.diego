628c0e71f32b81430cff9aa67654bbb512fe26d2
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-10-03 18:29:39 +0200

    Ejercicio terminado

---
+++ sesion-03/src/components/ImagenEstatica.vue
@@ -0,0 +1,8 @@
+<script setup>
+    
+</script>
+<template>
+    <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/monky-wilfred-sanya.jpg"></img>
+</template>
+<style scoped>
+</style>
