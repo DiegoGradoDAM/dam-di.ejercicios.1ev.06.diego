e46edea201407e9a803544f06bdbbea50fedb336
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-10-03 19:02:11 +0200

    Ejercicio terminado

---
+++ sesion-03/src/components/ParrafoFormato.vue
@@ -0,0 +1,15 @@
+<script setup>
+    
+</script>
+<template>
+   <p class="highlight text-large">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis culpa</p>
+</template>
+<style scoped>
+    .text-large{
+        font-size: 20px;
+        
+    }
+    .highlight{
+        background-color: yellow;
+    }
+</style>
