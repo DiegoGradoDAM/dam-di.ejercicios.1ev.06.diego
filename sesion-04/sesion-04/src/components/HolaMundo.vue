c63c37dc40ea52d631f6ab4077e100fc8253d4d4
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-10-09 20:57:01 +0200

    Ejercicio terminado

--- sesion-04/sesion-04/src/components/HolaMundo.vue
+++ sesion-04/sesion-04/src/components/HolaMundo.vue
@@ -4,19 +4,21 @@
     <h1>{{ datos.mensaje }}</h1>
     <p>Autor: {{ datos.autor }}</p>
     <p>Fecha: {{ datos.dia }}/{{ datos.mes }}/{{ datos.anio }}</p>
-   
+    {{ datos.contador }}
     <button @click="cambiarDatos">Cambiar Datos</button>
     </div>
-   </template>
-   <script setup>
-   import { reactive, onMounted } from 'vue';
+</template>
+<script setup>
+   import { reactive, onMounted, onUpdated, onBeforeMount,onUnmounted, onBeforeUpdate } from 'vue';
    // Usamos reactive para crear un objeto reactivo
+   let n = 0;
    const datos = reactive({
     mensaje: "¡Hola, Mundo!",
     autor: "Anónimo",
     dia: 1,
     mes: 1,
-    anio: 2023
+    anio: 2023,
+    contador: n
    });
    const cambiarDatos = () => {
     datos.mensaje = "¡Mensaje cambiado!";
@@ -24,10 +26,38 @@
     datos.dia = 15;
     datos.mes = 12;
     datos.anio = 2024;
+    datos.contador = n+1;
    };
+   //Funcion que se usara para que la hora cambie
+   function actualizarHora(){
+    let now = new Date();
+    //Parte para obtener el tiempo añadido a los mensajes
+    let hours = String(now.getHours()).padStart(2, '0');
+    let minutes = String(now.getMinutes()).padStart(2, '0');
+    let seconds = String(now.getSeconds()).padStart(2, '0');
+    let milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+
+    // Formatear el mensaje en "hh.mm.ss.ms"
+    return `${hours}.${minutes}.${seconds}.${milliseconds}`;
+   }
+   
+   onBeforeMount(()=> {
+    console.log("onBeforeMount -- "+actualizarHora());
+   });
    // Ciclo de vida: onMounted
    onMounted(() => {
     console.log("El componente HolaMundo ha sido montado");
+    console.log("onMounted -- "+actualizarHora());
    });
-   </script>
+   onUpdated(()=> {
+    console.log("onUpdated -- "+actualizarHora());
+    n = n+1;
+   })
+   onUnmounted(()=> {
+    console.log("onUnMounted -- "+actualizarHora());
+   })
+   onBeforeUpdate(()=> {
+    console.log("onBeforeUpdate -- "+actualizarHora());
+   })
+</script>
    
