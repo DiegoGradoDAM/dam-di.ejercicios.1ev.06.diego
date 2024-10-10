643feb6191535f87865bd46cc23d60e04bf87dd6
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-10-10 19:02:08 +0200

    Tutorial Terminado

--- sesion-05/src/App.vue
+++ sesion-05/src/App.vue
@@ -2,9 +2,19 @@
 <template>
   <div>
     <Tuto01 />
+    <Tuto0201/>
+    <Tuto0202/>
+    <Tuto0203/>
+    <Tuto0204/>
+    <Tuto0205/>
   </div>
  </template>
  <script setup>
   import Tuto01 from './components/Tuto01Ap01.vue';
+  import Tuto0201 from './components/Tuto02Ap01.vue';
+  import Tuto0202 from './components/Tuto02Ap02.vue';
+  import Tuto0203 from './components/Tuto02Ap03.vue';
+  import Tuto0204 from './components/Tuto02Ap04.vue';
+  import Tuto0205 from './components/Tuto02Ap05.vue';
  </script>
  
---
+++ sesion-05/src/components/Tuto02Ap01.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(nombre, index) in nombres" :key="index">{{ nombre }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup>
+   import { ref } from 'vue';
+   const nombres = ref(['Ana', 'Juan', 'María']);
+</script>
