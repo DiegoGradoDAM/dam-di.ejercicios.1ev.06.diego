d561afaea83e90d064a0c1cad8e7a22be4e96dcb
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-10-17 17:54:51 +0200

    Tutorial Terminado

--- sesion-05/src/App.vue
+++ sesion-05/src/App.vue
@@ -7,6 +7,11 @@
     <Tuto0203/>
     <Tuto0204/>
     <Tuto0205/>
+    <Tuto0301/>
+    <Tuto0302/>
+    <Tuto0303/>
+    <Tuto0304/>
+    <Tuto0305/>
   </div>
  </template>
  <script setup>
@@ -16,5 +21,10 @@
   import Tuto0203 from './components/Tuto02Ap03.vue';
   import Tuto0204 from './components/Tuto02Ap04.vue';
   import Tuto0205 from './components/Tuto02Ap05.vue';
+  import Tuto0301 from './components/Tuto03Ap01.vue';
+  import Tuto0302 from './components/Tuto03Ap02.vue';
+  import Tuto0303 from './components/Tuto03Ap03.vue';
+  import Tuto0304 from './components/Tuto03Ap04.vue';
+  import Tuto0305 from './components/Tuto03Ap05.vue';
  </script>
  
