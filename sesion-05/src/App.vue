949e7745d4ed241f7b33e028b00cf66a13d9a5e8
DiegoGradoDAM <plaza.diego.94@gmail.com>
2024-10-17 18:18:58 +0200

    Tutorial Terminado

--- sesion-05/src/App.vue
+++ sesion-05/src/App.vue
@@ -1,17 +1,17 @@
 <!-- App.vue -->
 <template>
   <div>
-    <Tuto0401/>
-    <Tuto0402/>
-    <Tuto0403/>
-    <Tuto0404/>
-    <Tuto0405/>
+    <Tuto0501/>
+    <Tuto0502/>
+    <Tuto0503/>
+    <Tuto0504/>
+    <Tuto0505/>
   </div>
  </template>
  <script setup>
-  import Tuto0401 from './components/Tuto04Ap01.vue';
-  import Tuto0402 from './components/Tuto04Ap02.vue';
-  import Tuto0403 from './components/Tuto04Ap03.vue';
-  import Tuto0404 from './components/Tuto04Ap04.vue';
-  import Tuto0405 from './components/Tuto04Ap05.vue';
+  import Tuto0501 from './components/Tuto05Ap01.vue';
+  import Tuto0502 from './components/Tuto05Ap02.vue';
+  import Tuto0503 from './components/Tuto05Ap03.vue';
+  import Tuto0504 from './components/Tuto05Ap04.vue';
+  import Tuto0505 from './components/Tuto05Ap05.vue';
  </script>
---
+++ sesion-05/src/components/Tuto05Ap03.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <img v-show="mostrarImagen" src="https://via.placeholder.com/150" alt="Imagen dinámica" />
+        <button @click="mostrarImagen = !mostrarImagen">Mostrar/Ocultar Imagen</button>
+    </div>
+</template>
+<script setup>
+   import { ref } from 'vue';
+   const mostrarImagen = ref(false);
+</script>
