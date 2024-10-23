4ae4a15c6b461606a5b1e5f464f58964bba7ff9f
DiegoGradoDAM <d.plaza@fpfempa.es>
2024-10-23 22:58:15 +0200

    Ejercicio Terminado

--- sesion-05/src/components/ListaTareas.vue
+++ sesion-05/src/components/ListaTareas.vue
@@ -2,18 +2,19 @@
     <input v-model="nuevaTarea" placeholder="Escribe una nueva tarea" />
     <button id="boton" v-if="nuevaTarea" @click="agregarTarea">Agregar Tarea</button>
     
-    <p v-if="tareas.length === 0">La lista esta vacia</p>
+    <p v-if="tareas.length === 0">No hay tareas disponibles</p>
     <ul>
-        <li v-for="(tarea, index) in tareas" :key="index">
+        <li v-for="(tarea, index) in tareas" :key="index" v-show="mostrarCompletadas || !tarea.completado">
             Tarea: {{ tarea.nombre }} - Completado: {{ tarea.completado }} 
             <input type="checkbox" v-model="tarea.completado" />
         </li>
     </ul>
+    <button @click="mostrarCompletadas = !mostrarCompletadas">Mostrar/Ocultar tareas completadas</button>
 </template>
 <script setup>
     import { ref, reactive } from 'vue';
     const tareas = ref([]);
-
+    const mostrarCompletadas = ref(true);
     const nuevaTarea = ref('');
 
     function agregarTarea(){
