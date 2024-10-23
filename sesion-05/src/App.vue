24527dde520f143862d456e29438ea0574e3e7f3
DiegoGradoDAM <d.plaza@fpfempa.es>
2024-10-23 22:30:54 +0200

    Prueba ordenador casa

--- sesion-05/src/App.vue
+++ sesion-05/src/App.vue
@@ -1,17 +1,9 @@
 <!-- App.vue -->
 <template>
   <div>
-    <Tuto0501/>
-    <Tuto0502/>
-    <Tuto0503/>
-    <Tuto0504/>
-    <Tuto0505/>
+    <ListaTareas/>
   </div>
- </template>
- <script setup>
-  import Tuto0501 from './components/Tuto05Ap01.vue';
-  import Tuto0502 from './components/Tuto05Ap02.vue';
-  import Tuto0503 from './components/Tuto05Ap03.vue';
-  import Tuto0504 from './components/Tuto05Ap04.vue';
-  import Tuto0505 from './components/Tuto05Ap05.vue';
- </script>
+</template>
+<script setup>
+  import ListaTareas from './components/ListaTareas.vue';
+</script>
---
+++ sesion-05/src/components/ListaTareas.vue
@@ -0,0 +1,26 @@
+<template>
+    <input v-model="nuevaTarea" placeholder="Escribe una nueva tarea" />
+    <button id="boton" v-if="nuevaTarea" @click="agregarTarea">Agregar Tarea</button>
+    
+    <p v-if="tareas.length === 0">La lista esta vacia</p>
+    <ul>
+        <li v-for="(tarea, index) in tareas" :key="index">
+            Tarea: {{ tarea.nombre }} - Completado: {{ tarea.completado }} 
+            <input type="checkbox" v-model="tarea.completado" />
+        </li>
+    </ul>
+</template>
+<script setup>
+    import { ref, reactive } from 'vue';
+    const tareas = ref([]);
+
+    const nuevaTarea = ref('');
+
+    function agregarTarea(){
+        const tarea = reactive({
+            nombre: nuevaTarea.value,       
+            completado: false 
+        });
+        tareas.value.push(tarea);
+    }
+</script>
